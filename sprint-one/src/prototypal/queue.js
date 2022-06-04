52f4546201e9c75c13799ffc0dc7140993078242
David Arredondo <dcarredondo90@gmail.com>
2022-06-03 23:38:46 -0700

    completed LinkedLists

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,7 +1,7 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  var newObj = Object.create(queueMethods)
+  var newObj = Object.create(queueMethods);
 
   newObj.dequeueSize = 0;
   newObj.enqueueSize = 0;
@@ -14,20 +14,20 @@ var queueMethods = {};
 queueMethods.enqueue = function (value) {
   queueMethods[this.enqueueSize] = value;
   this.enqueueSize++;
- }
+};
 //pop
 //dequeue
 queueMethods.dequeue = function () {
   if (this.enqueueSize - this.dequeueSize) {
-    var temp = queueMethods[this.dequeueSize]
-    delete queueMethods[this.dequeueSize]
+    var temp = queueMethods[this.dequeueSize];
+    delete queueMethods[this.dequeueSize];
     this.dequeueSize++;
-    return temp
+    return temp;
   }
-}
+};
 //
- //size
+//size
 queueMethods.size = function() {
 
   return this.enqueueSize - this.dequeueSize;
-}
+};
--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -23,13 +23,51 @@ var LinkedList = function() {
     var temp = list.head; //{value: 'b', next: null}
     //reassign head reference to next item
     list.head = list.head.next;// null
-
+    //return removed value
     return temp.value; //'b'
   };
-
-  list.contains = function(target) {
-
+  //                                                                                                                      temp.next = b
+  list.contains = function(target, currHead) { //                                                                               list.head.next
+    //returns boolean reflecting whether or not the passed-in value is in the linked list                  list.head
+    //head points to first node in list and contains a reference to next node follow nodes                 {value: 'a', next: 'b'},  {value: 'b', next: 'c'} , {value: 'c', next: 'd'}
+    //check if list.head is equal to target
+    if (list.head.value === target || list.tail.value === target) {
+    //if it is return true
+      return true;
+    //otherwise
+    }
+    if (list.head.value === list.tail.value) {
+      return false;
+    } else {
+      //reassign list.head to list.head.next
+      list.head = list.head.next;
+      list.contains(target, list.head);
+    }
+    return false;
+    //store list.head.next
+    //return boolean
+    // }
   };
+
+  // list.contains = function(target) {
+  //   // debugger;
+  //   var nodeStart = list.head;
+  //   console.log('nodeStart', nodeStart);
+  //   console.log('nodeStart.value:', nodeStart.value);
+  //   if (nodeStart.value === target) {
+  //     return true;
+  //   }
+
+  //   // include recursive somewhere
+  //   if (nodeStart.next !== null) {
+  //     console.log('nodeStart.next', nodeStart.next);
+  //     if (nodeStart.next.value === target) {
+  //       return true;
+  //     } else {
+  //       return false;
+  //     }
+  //   }
+  // };
 
   return list;
 };
